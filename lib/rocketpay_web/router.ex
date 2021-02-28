1a4cdf3924d168c202885cd912442ccea3f1344e
Jonathan Candido <jhlcandido@gmail.com>
2021-02-28 06:40:37 -0300

    users

--- lib/rocketpay_web/router.ex
+++ lib/rocketpay_web/router.ex
@@ -9,6 +9,7 @@ defmodule RocketpayWeb.Router do
     pipe_through :api
 
     get "/", WelcomeController, :index
+    post "/users", UsersController, :create
   end
 
   # Enables LiveDashboard only for development
